02d71d7f783668d37fcc2f0b5e7f28d55d90c59e
AlekseiKalashnik <id50538996@gmail.com>
2023-07-27 22:07:42 +0300

    change protocol and add jaas file

--- src/main/java/com/app/stock/kafkaservice/KafkaServiceApplication.java
+++ src/main/java/com/app/stock/kafkaservice/KafkaServiceApplication.java
@@ -10,6 +10,7 @@ public class KafkaServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(KafkaServiceApplication.class, args);
+        System.setProperty("java.security.auth.login.config","/jaas.conf");
     }
 
 }
