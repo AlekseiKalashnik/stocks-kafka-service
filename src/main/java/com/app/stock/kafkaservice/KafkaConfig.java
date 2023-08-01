97e3bbfb0520daee29264e35c8f4370b1005a0c9
AlekseiKalashnik <id50538996@gmail.com>
2023-08-01 19:15:34 +0300

    return

--- src/main/java/com/app/stock/kafkaservice/KafkaConfig.java
+++ src/main/java/com/app/stock/kafkaservice/KafkaConfig.java
@@ -1,40 +1,48 @@
-package com.app.stock.kafkaservice;
-
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.security.jaas.KafkaJaasLoginModuleInitializer;
-
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-
-@Configuration
-public class KafkaConfig {
-
-    String HOST = "rc1a-bfnhhricruq3oufj.mdb.yandexcloud.net:9091";
-    String USER = "developer";
-    String PASS = "11111111";
-    String TS_FILE = "/etc/security/ssl";
-    String TS_PASS = "11111111";
-    String jaasTemplate = "org.apache.kafka.common.security.scram.ScramLoginModule required username=\"%s\" password=\"%s\";";
-    String jaasCfg = String.format(jaasTemplate, USER, PASS);
-    String GROUP = "group-1";
-
-    @Bean
-    public KafkaJaasLoginModuleInitializer jaasConfig() throws IOException {
-        KafkaJaasLoginModuleInitializer jaasConfig = new KafkaJaasLoginModuleInitializer();
-        jaasConfig.setControlFlag(KafkaJaasLoginModuleInitializer.ControlFlag.valueOf("REQUIRED"));
-        Map<String, String> options = new HashMap<>();
-        options.put("bootstrap.servers", HOST);
-        options.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        options.put("group.id", GROUP);
-        options.put("security.protocol", "SASL_SSL");
-        options.put("sasl.mechanism", "SCRAM-SHA-512");
-        options.put("sasl.jaas.config", jaasCfg);
-        options.put("ssl.truststore.location", TS_FILE);
-        options.put("ssl.truststore.password", TS_PASS);
-        jaasConfig.setOptions(options);
-        return jaasConfig;
-    }
-}
+//package com.app.stock.kafkaservice;
+//
+//import com.app.stock.stockAnalyzer.dto.UserDTO;
+//import org.apache.kafka.clients.consumer.ConsumerConfig;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.kafka.core.ConsumerFactory;
+//import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+//import org.springframework.kafka.security.jaas.KafkaJaasLoginModuleInitializer;
+//
+//import java.io.IOException;
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//@Configuration
+//public class KafkaConfig {
+//
+//    String HOST = "rc1a-bfnhhricruq3oufj.mdb.yandexcloud.net:9091";
+//    String USER = "developer";
+//    String PASS = "11111111";
+//    String TS_FILE = "/etc/security/ssl";
+//    String TS_PASS = "11111111";
+//    String jaasTemplate = "org.apache.kafka.common.security.scram.ScramLoginModule required username=\"%s\" password=\"%s\";";
+//    String jaasCfg = String.format(jaasTemplate, USER, PASS);
+//    String GROUP = "group-1";
+//
+//    @Bean
+//    public KafkaJaasLoginModuleInitializer jaasConfig() throws IOException {
+//        KafkaJaasLoginModuleInitializer jaasConfig = new KafkaJaasLoginModuleInitializer();
+//        jaasConfig.setControlFlag(KafkaJaasLoginModuleInitializer.ControlFlag.valueOf("REQUIRED"));
+//        Map<String, String> options = new HashMap<>();
+//        options.put("bootstrap.servers", HOST);
+//        options.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+//        options.put("group.id", GROUP);
+//        options.put("security.protocol", "SASL_SSL");
+//        options.put("sasl.mechanism", "SCRAM-SHA-512");
+//        options.put("sasl.jaas.config", jaasCfg);
+//        options.put("ssl.truststore.location", TS_FILE);
+//        options.put("ssl.truststore.password", TS_PASS);
+//        jaasConfig.setOptions(options);
+//        return jaasConfig;
+//    }
+//
+//    @Bean
+//    public ConsumerFactory<KafkaJ> consumerFactory() throws IOException {
+//        return new DefaultKafkaConsumerFactory<>(jaasConfig());
+//    }
+//}
