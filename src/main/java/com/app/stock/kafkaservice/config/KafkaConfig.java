e52c6aa5ad84d9bdf54538fc74c21a251005f8d3
AlekseiKalashnik <id50538996@gmail.com>
2023-08-03 22:17:00 +0300

    attempt

--- src/main/java/com/app/stock/kafkaservice/config/KafkaConfig.java
+++ src/main/java/com/app/stock/kafkaservice/config/KafkaConfig.java
@@ -1,14 +1,20 @@
-//package com.app.stock.kafkaservice;
+//package com.app.stock.kafkaservice.config;
 //
 //import com.app.stock.stockAnalyzer.dto.UserDTO;
+//import org.apache.kafka.clients.consumer.Consumer;
 //import org.apache.kafka.clients.consumer.ConsumerConfig;
+//import org.apache.kafka.clients.consumer.KafkaConsumer;
+//import org.apache.kafka.common.config.SslConfigs;
+//import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.context.annotation.Bean;
 //import org.springframework.context.annotation.Configuration;
+//import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 //import org.springframework.kafka.core.ConsumerFactory;
 //import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 //import org.springframework.kafka.security.jaas.KafkaJaasLoginModuleInitializer;
 //
 //import java.io.IOException;
+//import java.util.Arrays;
 //import java.util.HashMap;
 //import java.util.Map;
 //
@@ -16,6 +22,7 @@
 //public class KafkaConfig {
 //
 //    String HOST = "rc1a-bfnhhricruq3oufj.mdb.yandexcloud.net:9091";
+//    String TOPIC = "topic-1";
 //    String USER = "developer";
 //    String PASS = "11111111";
 //    String TS_FILE = "/etc/security/ssl";
@@ -39,10 +46,31 @@
 //        options.put("ssl.truststore.password", TS_PASS);
 //        jaasConfig.setOptions(options);
 //        return jaasConfig;
+//
 //    }
 //
+//    @Autowired
+//    private ConsumerFactory<Integer, String> consumerFactory;
+//
+//    @Autowired
+//    private CryptoService cryptoService;
+//
 //    @Bean
-//    public ConsumerFactory<KafkaJ> consumerFactory() throws IOException {
-//        return new DefaultKafkaConsumerFactory<>(jaasConfig());
+//    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
+//        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+//        factory.setConsumerFactory(new DefaultKafkaConsumerFactory<>(consumerConfig()));
+//        return factory;
+//    }
+//
+//    private Map<String, Object> consumerConfig() {
+//        Map<String, Object> consumerConfig = new HashMap<>(consumerFactory.getConfigurationProperties());
+//        decryptAndAddToConsumerConfig(consumerConfig, SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG);
+//        decryptAndAddToConsumerConfig(consumerConfig, SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG);
+//        decryptAndAddToConsumerConfig(consumerConfig, SslConfigs.SSL_KEY_PASSWORD_CONFIG);
+//        return consumerConfig;
+//    }
+//
+//    private void decryptAndAddToConsumerConfig(Map<String, Object> config, String property) {
+//        config.compute(property, (k, v) -> cryptoService.decrypt((String) v));
 //    }
 //}
